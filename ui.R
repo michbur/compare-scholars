e7b0fedeb5c066f330e928fcab28f275cc3a7dda
michbur <michalburdukiewicz@gmail.com>
2022-09-29 18:26:41 +0200

    added citations per publication

--- ui.R
+++ ui.R
@@ -1,23 +1,29 @@
 library(shiny)
 library(shinycssloaders)
 library(DT)
+library(ggiraph)
 
 shinyUI(fluidPage(
 
-    titlePanel("Scholar"),
+  titlePanel("Scholar"),
 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("n_researcher", min = 1, max = 5, step = 1, value = 1,
-                        label = "Number of researchers"),
-            uiOutput("selection_boxes"),
-            actionButton(inputId = "run_btn", label = "Run!")
-        ),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("n_researcher", min = 1, max = 5, step = 1, value = 1,
+                  label = "Number of researchers"),
+      uiOutput("selection_boxes"),
+      actionButton(inputId = "run_btn", label = "Run!")
+    ),
 
-        mainPanel(
-            checkboxInput("cum_logical", "Cumulative values"),
-            withSpinner(plotOutput("comp_plot")),
-            withSpinner(dataTableOutput("comp_df"))
-        )
+    mainPanel(
+      checkboxInput("cum_logical", "Cumulative values"),
+      tabsetPanel(
+        tabPanel("Chart",
+                 withSpinner(girafeOutput("comp_plot"))),
+        tabPanel("Source data", withSpinner(dataTableOutput("comp_df")))
+      ),
+      withSpinner(dataTableOutput("cites_per_pub_df"))
+
     )
+  )
 ))
